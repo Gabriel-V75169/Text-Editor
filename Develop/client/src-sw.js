a92ca40f425f63c1cee483d9afe107280a4915e9
Gabriel Valencia <jrjr75169@gmail.com>
2023-12-08 13:56:38 -0800

    finished the config and src working on the client side js

---
+++ Develop/client/src-sw.js
@@ -0,0 +1,49 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+const ONE_MONTH_IN_SECONDS = 30 * 24 * 60 * 60;
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: ONE_MONTH_IN_SECONDS
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+
+
+
+registerRoute(
+  /\.(?:js|css|png|jpg|jpeg|gif|svg|webp)$/,
+  new StaleWhileRevalidate({
+    cacheName: 'static-assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_MONTH_IN_SECONDS
+      }),
+    ],
+  })
+  
+  );
